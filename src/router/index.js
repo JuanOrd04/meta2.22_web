05fac2217b5d14fbc28ea2d02cbb8ad2f2e62cf2
JuanOrd04 <orduna.juan@uabc.edu.mx>
2026-09-17 02:22:53 -0700

    configuracion inicial y api

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import IndexView from '../views/index.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: IndexView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About.vue')
+    }
+  ]
+})
+
+export default router
